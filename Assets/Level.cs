964a468749681c516fd30a4844a18352b4dd3d12
Caleb Clanton <Doondeka@gmail.com>
2018-08-05 00:39:42 -0400

    Version 0.0.0.4a
    Player can move, collide with platforms, slow down, and trigger falloff events.
    
    Next: Controls are disabled as camera turns to watch character fall in the distance and lens darkens. Level resets with one less life.

---
+++ Assets/Level.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class Level : MonoBehaviour {
+
+    public float lowestPoint;
+    private GameObject playerObject;
+    public UnityEvent OnFallOff = new UnityEvent();
+    
+    public void CheckFallOff()
+    {
+        Transform playerTransform = playerObject.transform;
+        Vector3 playerPosition = playerTransform.position;
+        float playerY = playerPosition.y;
+
+        if (playerY < lowestPoint)
+        {
+            OnFallOff.Invoke();
+        }
+    }
+
+    public void Start()
+    {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        OnFallOff.AddListener(delegate () { Debug.Log("Dead"); });
+    }
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerControl : MonoBehaviour
+{
+    private UnityEvent activePhase = new UnityEvent();
+    private UnityEvent onUpdate = new UnityEvent();
+    private UnityEvent onFall = new UnityEvent();
+    private Vector3 direction = new Vector2();
+    private Rigidbody rigidbody;
+    private Vector3 momentum { get { return direction * Speed; } }
+    private float Speed
+    {
+        get
+        {
+            switch (speedSetting)
+            {
+                case SpeedSetting.full: return fullSpeed;
+                case SpeedSetting.half: return halfSpeed;
+                default: return stopped;
+            }
+        }
+    }
+
+    public enum SpeedSetting { full, half, stopped }
+    public SpeedSetting speedSetting;
+    public bool canMove = true;
+    public const float fullSpeed = 2.5f;
+    public const float halfSpeed = 1.0f;
+    public const float stopped = 0.0f;
+
+    private void SubscribeToUnityEvent(UnityEvent targetEvent, UnityAction listener)
+    {
+        targetEvent.AddListener(listener);
+    }
+
+    private void SubscribeToActivePhase(UnityAction listener)
+    {
+        SubscribeToUnityEvent(activePhase, listener);
+    }
+
+    private void SubscribeToOnUpdate(UnityAction listener)
+    {
+        SubscribeToUnityEvent(onUpdate, listener);
+    }
+
+    public void SubscribeFallOffCheck()
+    {
+        Level level = GameObject.FindObjectOfType<Level>();
+        SubscribeToOnUpdate(level.CheckFallOff);
+    }
+
+    private void GetInput()
+    {
+        // If the player can't move then there's no point in getting any input.
+        if (!canMove)
+            return; 
+
+        // Gets vertical and horizontal input as 0-1 values then assigns them to direction.
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+        this.direction = new Vector3(xAxis, 0, yAxis);
+
+        // If shift is down then speedSetting is set to half, slowing the player; otherwise speed is at full.
+        bool shiftIsdown = Input.GetKey(KeyCode.LeftShift);
+        speedSetting = shiftIsdown == true ? SpeedSetting.half : SpeedSetting.full;
+    }
+
+    private void RollRigidbody()
+    {
+        this.rigidbody.angularVelocity = momentum;
+    }
+
+    public void EnableMovement()
+    {
+        SubscribeToActivePhase(GetInput);
+        SubscribeToActivePhase(RollRigidbody);
+    }
+
+    public void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();
+
+        EnableMovement();
+
+        SubscribeFallOffCheck();
+    }
+
+    public void Update()
+    {
+        activePhase.Invoke();
+
+        onUpdate.Invoke();
+    }
+}
